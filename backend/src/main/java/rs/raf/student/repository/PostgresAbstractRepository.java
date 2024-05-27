395b69a29a0c0f3791917156dee966235517c377
Nemanja Radovanovic <nradovanovic12022rn@raf.rs>
2024-05-28 00:46:13 +0200

    Backend | Implement abstract repository

---
+++ backend/src/main/java/rs/raf/student/repository/PostgresAbstractRepository.java
@@ -0,0 +1,35 @@
+package rs.raf.student.repository;
+
+import rs.raf.student.utils.Utilities;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.text.MessageFormat;
+import java.util.Properties;
+
+public class PostgresAbstractRepository {
+
+    private static Properties properties;
+
+    static {
+        try { Class.forName("com.mysql.cj.jdbc.Driver"); }
+        catch (ClassNotFoundException exception) { exception.printStackTrace(System.err); }
+
+        try { properties = Utilities.loadLocalProperties("application"); }
+        catch (Exception exception) { System.err.println("Local Application properties file is missing.\n" + exception.getMessage()); }
+    }
+
+    protected Connection createConnection() throws SQLException {
+
+        return DriverManager.getConnection(
+                MessageFormat.format("jdbc:postgresql://{0}:{1}/{2}", properties.getProperty("database.host"),
+                                                                      properties.getProperty("database.port"),
+                                                                      properties.getProperty("database.scheme")
+                ),
+                properties.getProperty("database.username"),
+                properties.getProperty("database.password")
+        );
+    }
+
+}
--- backend/src/main/java/rs/raf/student/utils/Utilities.java
+++ backend/src/main/java/rs/raf/student/utils/Utilities.java
@@ -1,11 +1,15 @@
 package rs.raf.student.utils;
 
+import lombok.SneakyThrows;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.digest.DigestUtils;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.security.SecureRandom;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Properties;
 import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -43,6 +47,24 @@ public class Utilities {
 
     private static String encodeBase64(byte[] bytes) {
         return new String(Base64.encodeBase64(bytes));
+    }
+
+    //endregion
+
+    //region Resource
+    private static final String separator = "/";
+
+    private static final String propertiesRoot = "/";
+
+    private static final String propertiesFile = ".properties";
+    private static final String localFile      = ".local";
+
+    public static Properties loadLocalProperties(String fileName) throws IOException {
+        Properties properties = new Properties();
+
+        properties.load(Utilities.class.getResourceAsStream(propertiesRoot + fileName + localFile + propertiesFile));
+
+        return properties;
     }
 
     //endregion
