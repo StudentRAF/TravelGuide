fb1811f5b7b957d6a59bb9ee8965b41fce53117a
Nemanja Radovanovic <nradovanovic12022rn@raf.rs>
2024-05-27 21:56:36 +0200

    Database | Refactor create script

--- database/create.sql
+++ database/create.sql
@@ -8,9 +8,7 @@ drop table if exists user_role;
 
 create table user_role(
     id   bigserial   primary key,
-    name varchar(32) not null,
-
-    check (name <> '')
+    name varchar(32) not null
 );
 
 create table "user"(
@@ -21,42 +19,29 @@ create table "user"(
     salt       char(24)     not null,
     password   char(44)     not null,
     role_id    bigint       not null references user_role(id),
-    enabled    boolean      not null default true,
-
-    check (first_name <> ''),
-    check (last_name  <> ''),
-    check (email      <> ''),
-    check (salt       <> ''),
-    check (password   <> '')
+    enabled    boolean      not null default true
 );
 
 
 create table activity(
     id   bigserial    primary key,
-    name varchar(256) not null unique,
-
-    check (name <> '')
+    name varchar(256) not null unique
 );
 
 create table destination(
     id          bigserial    primary key,
     name        varchar(256) not null unique,
-    description text         not null,
-
-    check (name <> '')
+    description text         not null
 );
 
 create table article(
     id             bigserial    primary key,
     title          varchar(256) not null,
     content        text         not null,
-    author         bigint       not null references "user"(id),
+    author_id      bigint       not null references "user"(id),
     destination_id bigint       not null references destination(id),
     created_at     date         not null default now(),
-    visits         int          not null default 0,
-
-    check (title   <> ''),
-    check (content <> '')
+    visits         int          not null default 0
 );
 
 create table article_activity(
@@ -70,7 +55,19 @@ create table comment(
     content    text      not null,
     author_id  bigint    not null references "user"(id),
     article_id bigint    not null references article(id),
-    created_at date      not null default now(),
-
-    check (content <> '')
+    created_at date      not null default now()
 );
+
+
+-- Constraint | Not Empty
+alter table user_role   add check (name       <> '');
+alter table "user"      add check (first_name <> '');
+alter table "user"      add check (last_name  <> '');
+alter table "user"      add check (email      <> '');
+alter table "user"      add check (salt       <> '');
+alter table "user"      add check (password   <> '');
+alter table activity    add check (name       <> '');
+alter table destination add check (name       <> '');
+alter table article     add check (title      <> '');
+alter table article     add check (content    <> '');
+alter table comment     add check (content    <> '');
