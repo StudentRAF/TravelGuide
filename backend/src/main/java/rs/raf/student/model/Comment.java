c05abaa2fedaae030007fd1360c00ad2f733a85b
Nemanja Radovanovic <nradovanovic12022rn@raf.rs>
2024-06-03 23:19:33 +0200

    Backend | add chain accessor (setter returns this) to models

--- backend/src/main/java/rs/raf/student/model/Comment.java
+++ backend/src/main/java/rs/raf/student/model/Comment.java
@@ -3,10 +3,12 @@ package rs.raf.student.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 import java.time.LocalDate;
 
 @Data
+@Accessors(chain = true)
 @AllArgsConstructor
 @NoArgsConstructor
 public class Comment {
--- backend/src/main/java/rs/raf/student/model/UserRole.java
+++ backend/src/main/java/rs/raf/student/model/UserRole.java
@@ -3,8 +3,10 @@ package rs.raf.student.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 @Data
+@Accessors(chain = true)
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserRole {
