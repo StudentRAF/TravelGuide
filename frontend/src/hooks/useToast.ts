5c5afcdcdd31d8b770c87b7658392d80ed257d04
Nemanja Radovanovic <nradovanovic12022rn@raf.rs>
2024-06-17 21:39:14 +0200

    Frontend | configure Toast hook

--- frontend/src/hooks/useToast.ts
+++ frontend/src/hooks/useToast.ts
@@ -6,8 +6,8 @@ import type {
   ToastProps,
 } from "@/components/Toast.tsx"
 
-const TOAST_LIMIT = 1
-const TOAST_REMOVE_DELAY = 1000000
+const TOAST_LIMIT = 2
+const TOAST_REMOVE_DELAY = 1000
 
 type ToasterToast = ToastProps & {
   id: string
