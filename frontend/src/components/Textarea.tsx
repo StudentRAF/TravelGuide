a155760d3c13d328aafaccafbebfce68ac2cfc29
Nemanja Radovanovic <nradovanovic12022rn@raf.rs>
2024-06-17 21:38:16 +0200

    Frontend | style Textarea component

--- frontend/src/components/Textarea.tsx
+++ frontend/src/components/Textarea.tsx
@@ -10,7 +10,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          "flex min-h-[80px] w-full rounded-large border border-gray-600 bg-transparent px-6 py-3 text-normal placeholder:text-gray-500 focus-visible:outline-none focus:border-gray-500 disabled:cursor-not-allowed disabled:bg-gray-850 resize-none [&::-webkit-scrollbar-track]:my-3 [&::-webkit-scrollbar-track]:rounded-full",
           className
         )}
         ref={ref}
