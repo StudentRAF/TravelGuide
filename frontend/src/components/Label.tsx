3a02a0179c1f846922ef23e1c63f475a9781018e
Nemanja Radovanovic <nradovanovic12022rn@raf.rs>
2024-06-17 03:32:32 +0200

    Frontend | style Label component

--- frontend/src/components/Label.tsx
+++ frontend/src/components/Label.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const labelVariants = cva(
-  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+  "text-normal text-gray-100 font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:text-gray-400"
 )
 
 const Label = React.forwardRef<
