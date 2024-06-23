67ec1814905d6eaab135c3acc04e97c8287ffb91
Nemanja Radovanovic <nradovanovic12022rn@raf.rs>
2024-06-23 14:37:09 +0200

    Backend | fix: preflight OPTIONS request returns status 401 - Unauthorized

--- backend/src/main/java/rs/raf/student/filter/AuthorizationFilter.java
+++ backend/src/main/java/rs/raf/student/filter/AuthorizationFilter.java
@@ -17,8 +17,14 @@ import java.util.List;
 import java.util.Map;
 
 import static jakarta.ws.rs.core.Response.Status;
-import static rs.raf.student.domain.HttpMethod.*;
-import static rs.raf.student.model.UserRole.*;
+import static rs.raf.student.domain.HttpMethod.DELETE;
+import static rs.raf.student.domain.HttpMethod.GET;
+import static rs.raf.student.domain.HttpMethod.OPTIONS;
+import static rs.raf.student.domain.HttpMethod.POST;
+import static rs.raf.student.domain.HttpMethod.PUT;
+import static rs.raf.student.model.UserRole.ADMIN;
+import static rs.raf.student.model.UserRole.ANYONE;
+import static rs.raf.student.model.UserRole.EDITOR;
 
 @Provider
 @PreMatching
@@ -32,33 +38,33 @@ public class AuthorizationFilter implements ContainerRequestFilter {
             PathRequest.of(GET,  "/users/id"   ), List.of(ADMIN),
             PathRequest.of(POST, "/users"      ), List.of(ADMIN),
             PathRequest.of(PUT,  "/users"      ), List.of(ADMIN),
-            PathRequest.of(POST, "/users/login"), List.of(ADMIN, EDITOR, ANYONE)
+            PathRequest.of(POST, "/users/login"), List.of(ANYONE)
         ));
 
         authorizationMap.putAll(Map.of(
-            PathRequest.of(GET,    "/destinations"   ), List.of(ADMIN, EDITOR, ANYONE),
-            PathRequest.of(GET,    "/destinations/id"), List.of(ADMIN, EDITOR, ANYONE),
+            PathRequest.of(GET,    "/destinations"   ), List.of(ANYONE),
+            PathRequest.of(GET,    "/destinations/id"), List.of(ANYONE),
             PathRequest.of(POST,   "/destinations"   ), List.of(ADMIN, EDITOR),
             PathRequest.of(PUT,    "/destinations"   ), List.of(ADMIN, EDITOR),
             PathRequest.of(DELETE, "/destinations"   ), List.of(ADMIN, EDITOR)
         ));
 
         authorizationMap.putAll(Map.of(
-            PathRequest.of(GET,    "/articles"               ), List.of(ADMIN, EDITOR, ANYONE),
-            PathRequest.of(GET,    "/articles/id"            ), List.of(ADMIN, EDITOR, ANYONE),
-            PathRequest.of(GET,    "/articles/destination/id"), List.of(ADMIN, EDITOR, ANYONE),
-            PathRequest.of(GET,    "/articles/activity/id"   ), List.of(ADMIN, EDITOR, ANYONE),
+            PathRequest.of(GET,    "/articles"               ), List.of(ANYONE),
+            PathRequest.of(GET,    "/articles/id"            ), List.of(ANYONE),
+            PathRequest.of(GET,    "/articles/destination/id"), List.of(ANYONE),
+            PathRequest.of(GET,    "/articles/activity/id"   ), List.of(ANYONE),
             PathRequest.of(POST,   "/articles"               ), List.of(ADMIN, EDITOR),
             PathRequest.of(PUT,    "/articles"               ), List.of(ADMIN, EDITOR),
             PathRequest.of(DELETE, "/articles"               ), List.of(ADMIN, EDITOR)
         ));
 
         authorizationMap.putAll(Map.of(
-            PathRequest.of(POST, "/comments"), List.of(ADMIN, EDITOR, ANYONE)
+            PathRequest.of(POST, "/comments"), List.of(ANYONE)
         ));
 
         authorizationMap.putAll(Map.of(
-            PathRequest.of(GET, "/activities"), List.of(ADMIN, EDITOR, ANYONE)
+            PathRequest.of(GET, "/activities"), List.of(ANYONE)
         ));
     }
 
@@ -70,6 +76,9 @@ public class AuthorizationFilter implements ContainerRequestFilter {
         HttpMethod method = HttpMethod.find(requestContext.getMethod());
         String     path   = requestContext.getUriInfo()
                                           .getPath();
+
+        if (OPTIONS.equals(method))
+            return;
 
         List<UserRole> authorizedRoles = authorizationMap.get(PathRequest.of(method, path));
 
